18417bf163aa53cbd444e6db70e21e4aa1e71b2d
kamabokochan <h.sasaki.worksfront@gmail.com>
2022-02-16 00:39:01 +0900

    prismjs追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import type { NextPage } from "next";
+import Prism from "prismjs";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  return <div>Welcome to Next.js!</div>
+  useEffect(() => {
+    Prism.highlightAll();
+  });
+  const [text, setText] = useState('const great = () => {console.log("Awesome");};');
+  return (
+    <>
+      <textarea
+        name="content"
+        onChange={(e) => {
+          return setText(e.target.value);
+        }}
+        value={text}
+      ></textarea>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<pre data-label="index.html"><code class="lang-javascript">${text}</code></pre>`,
+        }}
+      ></div>
+    </>
+  );
 };
 
 export default Home;
