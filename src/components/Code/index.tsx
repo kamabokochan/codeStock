c16cd3e5b431771b5f98664073d20e5437e29618
kamabokochan <h.sasaki.worksfront@gmail.com>
2022-02-27 16:12:19 +0900

    不要なコメントを削除

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -1,16 +1,16 @@
-// (1) import層
 import Prism from "prismjs";
 import React, { useEffect, useState } from "react";
 // import styled from "styled-components";
-// (2) Types層
+
 export type ContainerProps = {
   isEditable: boolean;
 };
+
 type ComponentProps = {
   text: string;
   setText: React.Dispatch<React.SetStateAction<string>>;
 } & ContainerProps;
-// (3) DOM層
+
 const Component: React.FC<ComponentProps> = (props) => {
   const { text, setText, isEditable } = props;
   return (
@@ -18,23 +18,23 @@ const Component: React.FC<ComponentProps> = (props) => {
       {isEditable ? (
         <textarea
           name="content"
-          className="prism-live lang-javascript"
+          className="prism-live lang-typescript"
           onChange={(e) => {
             return setText(e.target.value);
           }}
           value={text}
         ></textarea>
       ) : (
-        <pre data-label="index.js">
-          <code className="lang-javascript">{text}</code>
+        <pre>
+          <code className="lang-typescript">{text}</code>
         </pre>
       )}
     </div>
   );
 };
-// (4) Style層
+
 // const StyledComponent = styled(Component)`...`;
-// (5) Container層
+
 export const Code: React.FC<ContainerProps> = ({ isEditable }) => {
   useEffect(() => {
     Prism.highlightAll();
