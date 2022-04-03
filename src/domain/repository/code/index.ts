1146b174a8d68e19b112fddbe4bf41ad694e2a73
yamam00s <s.yamamo0327@gmail.com>
2022-04-03 19:14:15 +0900

    create repository

---
+++ src/domain/repository/code/index.ts
@@ -0,0 +1,17 @@
+import { Code } from "~/domain/entity";
+import { CodeDriver } from "~/infrastructure/supabase";
+
+export class CodeRepository {
+  private driver: CodeDriver;
+  constructor() {
+    this.driver = new CodeDriver();
+  }
+
+  async fetchAllCodes() {
+    const codesResponse = await this.driver.fetchAllCodes();
+    return codesResponse?.map((codeResponse) => {
+      const { id, title, description, code } = codeResponse;
+      new Code(id, title, description, code);
+    });
+  }
+}
